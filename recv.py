d0fa2c8d107c180d365a87160310e43fd04a9d7b
Joe Linoff <jlinoff@gmail.com>
2017-03-02 10:48:24 -0800

    Update to support command line arguments

--- recv.py
+++ recv.py
@@ -7,61 +7,183 @@ Receives messages send by the send.py program at periodic intervals.
 Here is an example that receives messages from any IPv4 address on
 port 8500.
 
-Command to open the port:
+   $ firewall-cmd --zone=public --add-port=8500/tcp
+   $ recv.py --host 0.0.0.0 --port 8500
+
+It will receive data sent by this command.
 
    $ firewall-cmd --zone=public --add-port=8500/tcp
-   $ recv.py 0.0.0.0 8500
-
-The optional arguments are:
-
-   HOST  - hostname, default 127.0.0.1
-   PORT  - port, default 8500
-   SECS  - seconds to pause, default 0.200 (200ms).
+   $ send.py --host 127.0.0.1 --port 8500
 '''
+import argparse
+import inspect
 import json
+import os
 import socket
 import sys
 import time
 
 
-HOST = '0.0.0.0'  # Listen on all NICs.
-PORT = 8500
+VERSION = '1.0.1'
 
 
-def create_listener(host, port):
+def infov(opts, msg, lev=1):
+    '''
+    Print a verbose message.
+    '''
+    if opts.verbose > 0:
+        print('INFO:{} {}'.format(inspect.stack()[lev][2], msg))
+
+
+def getopts():
+    '''
+    Process the command line arguments.
+    '''
+    # Trick to capitalize the built-in headers.
+    # Unfortunately I can't get rid of the ":" reliably.
+    def gettext(s):
+        lookup = {
+            'usage: ': 'USAGE:',
+            'positional arguments': 'POSITIONAL ARGUMENTS',
+            'optional arguments': 'OPTIONAL ARGUMENTS',
+            'show this help message and exit': 'Show this help message and exit.\n ',
+        }
+        return lookup.get(s, s)
+
+    argparse._ = gettext  # to capitalize help headers
+    base = os.path.basename(sys.argv[0])
+    name = os.path.splitext(base)[0]
+    usage = '\n  {0} [OPTIONS] <DOT_FILE>'.format(base)
+    desc = 'DESCRIPTION:{0}'.format('\n  '.join(__doc__.split('\n')))
+    epilog = r'''EXAMPLES:
+   # Example 1: help
+   $ {0} -h
+
+   # Example 2: simple receive
+   $ {0}
+
+   # Example 3: poll at 2 second intervals.
+   $ {0} -t 2
+
+   # Example 4: increase the receive packet size.
+   #            you would probably want to increase the size for the
+   #            sender as well.
+   $ {0} -s 4096
+
+   # Example 5: specify the host and port explicitly.
+   $ {0} -H other_host -p 8601
+ '''.format(base)
+    afc = argparse.RawTextHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=afc,
+                                     description=desc[:-2],
+                                     usage=usage,
+                                     epilog=epilog)
+
+    parser.add_argument('-b', '--backlog',
+                        action='store',
+                        type=int,
+                        default=5,
+                        metavar=('NUMBER'),
+                        help='''The socket listen backlog.
+Default: %(default)s.
+ ''')
+
+    parser.add_argument('-H', '--host',
+                        action='store',
+                        type=str,
+                        default='0.0.0.0',
+                        metavar=('HOST'),
+                        help='''The host.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-p', '--port',
+                        action='store',
+                        type=int,
+                        default=8500,
+                        metavar=('PORT'),
+                        help='''The port.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-q', '--quiet',
+                        action='store_true',
+                        help='''Don't display the messages as they are received.
+ ''')
+
+    parser.add_argument('-s', '--size',
+                        action='store',
+                        type=int,
+                        default=1024,
+                        metavar=('SIZE'),
+                        help='''Receive packet size.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-t', '--time',
+                        action='store',
+                        type=float,
+                        default=0.200,
+                        metavar=('SECONDS'),
+                        help='''The time to pause between receive operations.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-v', '--verbose',
+                        action='count',
+                        default=0,
+                        help='''Increase the level of verbosity.
+ ''')
+
+    parser.add_argument('-V', '--version',
+                        action='version',
+                        version='%(prog)s version {0}'.format(VERSION),
+                        help="""Show program's version number and exit.
+ """)
+
+    opts = parser.parse_args()
+    return opts
+
+
+def create_listener(opts):
     '''
     Create the listener.
     '''
-    sock_addr = (host, port)
+    infov(opts, 'creating the listener')
+    sock_addr = (opts.host, opts.port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(sock_addr)
-    sock.listen(5)
+    sock.listen(opts.backlog)
     return sock
 
 
-def receive_recs(host, port, pause):
+def receive_recs(opts):
     '''
     Poll for records.
     '''
-    listener = create_listener(host, port)
+    infov(opts, 'receiving records')
+    listener = create_listener(opts)
     while True:
         conn, addr = listener.accept()
         json_rec = conn.recv(1024)
         rec = json.loads(json_rec.decode('utf-8'))  # same format as sender
-        print('RCV: {}  {}  {}'.format(addr, rec['time'], rec['data']))
+        if not opts.quiet:
+            print('RCV: {}  {}  {}'.format(addr, rec['time'], rec['data']))
         conn.close()
-        time.sleep(pause)
+        time.sleep(opts.time)
 
 
 def main():
     '''
     Main receive loop.
     '''
-    host = HOST if len(sys.argv) < 2 else sys.argv[1]
-    port = PORT if len(sys.argv) < 3 else int(sys.argv[2])
-    secs = 0.200 if len(sys.argv) < 4 else float(sys.argv[3])
-    receive_recs(host, port, secs)
+    opts = getopts()
+    try:
+        receive_recs(opts)
+    except KeyboardInterrupt:
+        print('')
+    infov(opts, 'done')
 
 
 if __name__ == '__main__':
--- send.py
+++ send.py
@@ -7,16 +7,18 @@ Sends messages over a specific port to a host at periodic intervals.
 Here is an example that sends messages to recv_host on port 8500.
 
    $ firewall-cmd --zone=public --add-port=8500/tcp
-   $ send.py recv_host 8500
+   $ send.py --host recv_host --port 8500
 
-The optional arguments are:
+Here is what you would run on the recv_host:
 
-   HOST  - hostname, default 127.0.0.1
-   PORT  - port, default 8500
-   SECS  - seconds to pause, default 1.
+   $ firewall-cmd --zone=public --add-port=8500/tcp
+   $ recv.py --host 0.0.0.0 --port 8500
 '''
+import argparse
 import datetime
+import inspect
 import json
+import os
 import random
 import socket
 import string
@@ -24,31 +26,151 @@ import sys
 import time
 
 
-HOST = '127.0.0.1'
-PORT = 8500
+VERSION = '1.0.1'
 
 
-def create_record():
+def infov(opts, msg, lev=1):
+    '''
+    Print a verbose message.
+    '''
+    if opts.verbose > 0:
+        print('INFO:{} {}'.format(inspect.stack()[lev][2], msg))
+
+
+def getopts():
+    '''
+    Process the command line arguments.
+    '''
+    # Trick to capitalize the built-in headers.
+    # Unfortunately I can't get rid of the ":" reliably.
+    def gettext(s):
+        lookup = {
+            'usage: ': 'USAGE:',
+            'positional arguments': 'POSITIONAL ARGUMENTS',
+            'optional arguments': 'OPTIONAL ARGUMENTS',
+            'show this help message and exit': 'Show this help message and exit.\n ',
+        }
+        return lookup.get(s, s)
+
+    argparse._ = gettext  # to capitalize help headers
+    base = os.path.basename(sys.argv[0])
+    name = os.path.splitext(base)[0]
+    usage = '\n  {0} [OPTIONS] <DOT_FILE>'.format(base)
+    desc = 'DESCRIPTION:{0}'.format('\n  '.join(__doc__.split('\n')))
+    epilog = r'''EXAMPLES:
+   # Example 1: help
+   $ {0} -h
+
+   # Example 2: simple send
+   $ {0}
+
+   # Example 3: send at 2 second intervals.
+   $ {0} -t 2
+
+   # Example 4: increase the send packet size.
+   #            you would probably want to increase the size for the
+   #            receiver as well.
+   $ {0} -s 4096
+
+   # Example 5: specify the host and port explicitly.
+   $ {0} -H other_host -p 8601
+ '''.format(base)
+    afc = argparse.RawTextHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=afc,
+                                     description=desc[:-2],
+                                     usage=usage,
+                                     epilog=epilog)
+
+    parser.add_argument('-H', '--host',
+                        action='store',
+                        type=str,
+                        default='127.0.0.1',
+                        metavar=('HOST'),
+                        help='''The host.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-p', '--port',
+                        action='store',
+                        type=int,
+                        default=8500,
+                        metavar=('PORT'),
+                        help='''The port.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-q', '--quiet',
+                        action='store_true',
+                        help='''Don't display the messages as they are received.
+ ''')
+
+    parser.add_argument('-s', '--size',
+                        action='store',
+                        type=int,
+                        default=32,
+                        metavar=('SIZE'),
+                        help='''Send packet data size.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-r', '--rsize',
+                        action='store',
+                        type=int,
+                        default=1024,
+                        metavar=('SIZE'),
+                        help='''The response packet data size.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-t', '--time',
+                        action='store',
+                        type=float,
+                        default=1,
+                        metavar=('SECONDS'),
+                        help='''The time to pause between send operations.
+Default %(default)s.
+ ''')
+
+    parser.add_argument('-v', '--verbose',
+                        action='count',
+                        default=0,
+                        help='''Increase the level of verbosity.
+ ''')
+
+    parser.add_argument('-V', '--version',
+                        action='version',
+                        version='%(prog)s version {0}'.format(VERSION),
+                        help="""Show program's version number and exit.
+ """)
+
+    opts = parser.parse_args()
+    return opts
+
+
+def create_record(opts):
     '''
     Create a record to send.
     '''
-    data = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32))
+    infov(opts, 'create record with {} bytes of data'.format(opts.size))
+    data = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(opts.size))
     rec = {'data': data, 'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),}
     json_rec = json.dumps(rec)
     return json_rec.encode('ascii')
 
 
-def send(rec, host, port):
+def send(opts, rec):
     '''
     Send the record.
     '''
+    infov(opts, 'sending {}'.format(rec))
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    addr = (host, port)
-    print('SND: {}  {}'.format(addr, rec))
+    addr = (opts.host, opts.port)
+    if not opts.quiet:
+        print('SND: {}  {}'.format(addr, rec))
     try:
         sock.connect(addr)
         sock.sendall(bytes(rec))
-        response = sock.recv(1024)
+        response = sock.recv(opts.rsize)
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
     except socket.error:  # need to change this for Python 3
@@ -59,13 +181,15 @@ def main():
     '''
     Main send loop.
     '''
-    host = HOST if len(sys.argv) < 2 else sys.argv[1]
-    port = PORT if len(sys.argv) < 3 else int(sys.argv[2])
-    secs = 1 if len(sys.argv) < 4 else float(sys.argv[3])
-    while True:
-        rec = create_record()
-        send(rec, host, port)
-        time.sleep(secs)
+    opts = getopts()
+    try:
+        while True:
+            rec = create_record(opts)
+            send(opts, rec)
+            time.sleep(opts.time)
+    except KeyboardInterrupt:
+        print('')
+    infov(opts, 'done')
 
 
 if __name__ == '__main__':
